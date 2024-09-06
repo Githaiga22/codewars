93103331a2ca38ebe083abba06acfa6bb4090eb7
allkamau <allangithaiga5@gmail.com>
2024-09-06 14:12:57 +0300

    Add a program that takes a string and returns the 3rd argument

---
+++ ThirdTimeIsACharm/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Print(ThirdTimeIsACharm("123456789"))
+	fmt.Print(ThirdTimeIsACharm(""))
+	fmt.Print(ThirdTimeIsACharm("a b c d e f"))
+	fmt.Print(ThirdTimeIsACharm("12"))
+}
+
+func ThirdTimeIsACharm(str string) string {
+	if str == "" {
+		return "\n"
+	}
+	if len(str) < 3 {
+		return "\n"
+	}
+
+	result := ""
+
+	for i := 2; i < len(str); i +=3 {
+
+			result += string(str[i])
+	
+	
+	}
+	return result + "\n"
+}
