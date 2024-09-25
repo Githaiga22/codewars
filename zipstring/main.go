62c69ab5227623292f4b42e5aa8c7114af185d09
allkamau <allangithaiga5@gmail.com>
2024-09-25 09:38:03 +0300

    Update function

--- zipstring/main.go
+++ zipstring/main.go
@@ -11,37 +11,54 @@ func main() {
 	fmt.Println(ZipString("Helloo Therre!"))
 }
 
-func ZipString(s string) string {
-	// if string is empty return empty string
-if len(s) == 0 {
-	return ""
-}
-//initialize the result to store the string
-//initialize the previous char as the first index so as to compare repeating char
-//initialize the count to 1 
-result := ""
-previouschar := s[0]
-count := 1
+// func ZipString(s string) string {
+// 	// if string is empty return empty string
+// if len(s) == 0 {
+// 	return ""
+// }
+// //initialize the result to store the string
+// //initialize the previous char as the first index so as to compare repeating char
+// //initialize the count to 1
+// result := ""
+// previouschar := s[0]
+// count := 1
 
-//loop through the string from the second char
+// //loop through the string from the second char
+// for i := 1; i < len(s); i++ {
+// 	//initialize the current char so as to enable comparison
+// 	currentchar := s[i]
+// 	//if current char is same as previous char the count should increase
+// 	if previouschar == currentchar {
+// 		count++
+// 		//else if they dont repeat, add the count and the previouschar in the result
+// 		//remember to convert the count(int) to a string and previouschar(byte) to a string
+// 	} else {
+// 		result += strconv.Itoa(count) + string(previouschar)
+// 		//reset count to 1 for allowing next number
+// 		count = 1
+// 		//update the previous char is now current char
+// 		previouschar = currentchar
+// 	}
+// }
+// //store now the remaining char which is left out at the end
+// result += strconv.Itoa(count) + string(previouschar)
+// return result
+
+// }
+
+func ZipString(s string) string {
+count := 1
+res := ""
+
 for i := 1; i < len(s); i++ {
-	//initialize the current char so as to enable comparison
-	currentchar := s[i]
-	//if current char is same as previous char the count should increase
-	if previouschar == currentchar {
+	if s[i] == s[i-1] {
 		count++
-		//else if they dont repeat, add the count and the previouschar in the result
-		//remember to convert the count(int) to a string and previouschar(byte) to a string
 	} else {
-		result += strconv.Itoa(count) + string(previouschar)
-		//reset count to 1 for allowing next number
+		res+= strconv.Itoa(count) + string(s[i-1])
 		count = 1
-		//update the previous char is now current char
-		previouschar = currentchar
 	}
 }
-//store now the remaining char which is left out at the end
-result += strconv.Itoa(count) + string(previouschar)
-return result
-
+res+= strconv.Itoa(count) + string(s[len(s)-1])
+return res
 }
+
