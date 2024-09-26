f9f01eee06fbba4ecfd00bca3ec6c201955fa0ce
allkamau <allangithaiga5@gmail.com>
2024-09-26 23:43:11 +0300

    add function that returns count of similar char

---
+++ loops/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(CountChar("Hello World", 'l'))
+	fmt.Println(CountChar("5  balloons", 5))
+	fmt.Println(CountChar("   ", ' '))
+	fmt.Println(CountChar("The 7 deadly sins", '7'))
+}
+
+func CountChar(str string, c rune) int {
+count := 0
+if str == "" {
+	return 0
+}
+for _, char := range str {
+
+	if char == c {
+		count++
+	}
+
+}
+return count
+}
